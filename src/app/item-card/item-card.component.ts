b99d96e571b7266b1afdd5418d529b8929538df9
Ankit Singh <ankit20september@gmail.com>
2025-11-13 22:00:07 +0530

    Update Default Image

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -11,7 +11,7 @@ export class ItemCardComponent implements OnInit {
   @Input() isSearch = false;
   @Input() productDetails!: any;
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  placeholderImage = 'https://images.unsplash.com/photo-1553062407-98eeb64c6a62?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  placeholderImage = 'assets/images/no-product-image.png';
 
   @Output() goToProduct = new EventEmitter<any>();
 
